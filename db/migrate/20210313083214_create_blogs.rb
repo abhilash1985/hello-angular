d5e7961ca3db8a3b8ec81885dbe2207963707493
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-03-13 21:31:28 +0530

    Scaffolded blogs

---
+++ db/migrate/20210313083214_create_blogs.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# CreateBlogs
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
